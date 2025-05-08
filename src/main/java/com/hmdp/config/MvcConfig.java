79bbb07767793183a0c7aaa0ee82cae001350e00
pamrock <1245945665@qq.com>
2025-05-08 16:11:44 +0800

    项目初始文件

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName MvcConfig
+ * @Description TODO
+ * @Author 12459
+ * @Date 2025/5/8 15:52
+ **/
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/user/code",
+                        "/user/login",
+                        "/bolg/hot",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/coucher/**");
+    }
+}
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.hmdp.utils;
+
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @ClassName LoginInterceptor
+ * @Description 登录拦截
+ * @Author 12459
+ * @Date 2025/5/8 15:42
+ **/
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取session
+        HttpSession session = request.getSession();
+        //获取session中的用户
+        Object user = session.getAttribute("user");
+        if (user == null) {
+            response.setStatus(401);
+            return false;
+        }
+        //用户存在，保存用户信息到threadlocal
+        UserHolder.saveUser((User) user);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
