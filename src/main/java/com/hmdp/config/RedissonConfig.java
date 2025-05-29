b3b57662c987e7392454a26017b094e7debe4d8b
pamrock <1245945665@qq.com>
2025-05-29 19:40:03 +0800

    使用redisson优化秒杀

---
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -0,0 +1,24 @@
+package com.hmdp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName RedissonConfig
+ * @Description redisson配置
+ * @Author 12459
+ * @Date 2025/5/28 18:41
+ **/
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+}
